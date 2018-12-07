5c7d71414a22965f1248510aa4f9697f3952b10e
Luis Fuentes <fuentesld@gmail.com>
2018-12-07 10:59:33 -0600

    Se añadio geologalizacion

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
-const argv = require("yargs").options({
-  direccion: {
-    alias: "d",
-    describe: "Nombre de la Ciudad",
-    demand: true
-  }
+const axios = require('axios');
+const argv = require('yargs').options({
+	direccion: {
+		alias: 'd',
+		describe: 'Nombre de la Ciudad',
+		demand: true
+	}
 }).argv;
 
 console.log(argv);
+
+let encodedUrl = encodeURI(argv.direccion);
+axios
+	.get(
+		`https://maps.googleapis.com/maps/api/geocode/json?address=${encodedUrl}&key=AIzaSyBbVLEqTQrgC6gHoMIQQ3NfebX7o6MgmtU`
+	)
+	.then(respuesta => {
+		let localidad = respuesta.data.results[0].formatted_address;
+		let coordenadas = respuesta.data.results[0].geometry.location;
+		console.log(localidad);
+		console.log(coordenadas.lat);
+		console.log(coordenadas.lng);
+	})
+	.catch(e => console.log('ERROR !!!!'));
